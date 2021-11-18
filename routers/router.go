86f66e636641571e6951e38fa11ecc2ed45ddd2b
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2021-11-18 19:35:28 +0700

    New Code Movie

---
+++ routers/router.go
@@ -0,0 +1,38 @@
+package routers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func Init() *fiber.App {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(compress.New())
+	app.Static("/", "frontend/public", fiber.Static{
+		Compress:  true,
+		ByteRange: true,
+		Browse:    true,
+	})
+	app.Get("/ipaddress", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status":      fiber.StatusOK,
+			"message":     "Success",
+			"record":      "data",
+			"BASEURL":     c.BaseURL(),
+			"HOSTNAME":    c.Hostname(),
+			"IP":          c.IP(),
+			"IPS":         c.IPs(),
+			"OriginalURL": c.OriginalURL(),
+			"Path":        c.Path(),
+			"Protocol":    c.Protocol(),
+			"Subdomain":   c.Subdomains(),
+		})
+	})
+	app.Get("/dashboard", monitor.New())
+	return app
+}
