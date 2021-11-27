df574f40dff8a45c9b83e2749a6d053d158fc74a
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2021-11-27 18:07:47 +0700

    Update Footer Hitstat

--- frontend/src/component/Footer.svelte
+++ frontend/src/component/Footer.svelte
@@ -3,11 +3,18 @@
         <img width="150" src="https://duniafilm.b-cdn.net/img/download.png" alt="">
     </a>
     <br>
-    <!-- Histats.com  START (html only)-->
-    <a href="/" alt="page hit counter" target="_blank" >
-        <embed src="//s10.histats.com/429.swf"  flashvars="jver=1&acsid=4606934&domi=4"  quality="high"  width="112" height="75" name="429.swf"  align="middle" type="application/x-shockwave-flash" pluginspage="//www.macromedia.com/go/getflashplayer" wmode="transparent" />
-    </a>
-    <img  src="//sstatic1.histats.com/0.gif?4606934&101" alt="" border="0">
+       <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+    <script type="text/javascript">var _Hasync= _Hasync|| [];
+    _Hasync.push(['Histats.start', '1,4606934,4,429,112,75,00011111']);
+    _Hasync.push(['Histats.fasi', '1']);
+    _Hasync.push(['Histats.track_hits', '']);
+    (function() {
+    var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+    hs.src = ('//s10.histats.com/js15_as.js');
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+    })();</script>
+    <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4606934&101" alt="" border="0"></a></noscript>
     <!-- Histats.com  END  -->
     <br>
     Copyright © 2021 ISB FILM. All Right Reserved.
